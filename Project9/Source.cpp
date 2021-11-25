d956c6e4660c104c96f5c3291115af47a850f1dd
rkorotkiy <korotkiy.roman@mail.ru>
2021-11-25 11:57:09 +0300

    .

--- Project9/Source.cpp
+++ Project9/Source.cpp
@@ -4,6 +4,8 @@
 #include <libloaderapi.h>
 #include "pkcs11.h"
 #include "tdef.h"
+#include "PKCSExceptions.h"
+#include <string>
 
 class CryptoToken {
 private:
@@ -36,162 +38,100 @@ public:
 };
 
 CryptoToken::CryptoToken(const wchar_t* PATH_TO_DLL) {
-	try {
-		hLib = LoadLibrary(PATH_TO_DLL);
-		if (hLib == NULL) throw  1;
-	}
-	catch (int a) {
-		std::cout << "Error loading library" << std::endl;
-		exit(1);
-	}
+	hLib = LoadLibrary(PATH_TO_DLL);
 }
 
 void CryptoToken::LoadProc(HINSTANCE hLib, const char* FUNC_NAME) {
-	try {
-		LoadFunc = (void*)GetProcAddress(hLib, FUNC_NAME);
-		if (LoadFunc == NULL) throw 1;
-	}
-	catch (int a) {
-		std::cout << "Error loading function" << std::endl;
-		exit(1);
-	}
+	LoadFunc = (void*)GetProcAddress(hLib, FUNC_NAME);
 }
 
 int CryptoToken::GetFunctionList() {
+	if (hLib == NULL) throw  LibLoadErr();
 	LoadProc(hLib, "C_GetFunctionList");
+	if (LoadFunc == NULL) throw FuncLoadErr();
 	int (*C_GetFuncList)(CK_FUNCTION_LIST_PTR_PTR);
 	C_GetFuncList = (C_GetFunctionList_decl)LoadFunc;
-	try {
-		CK_RV rv = C_GetFuncList(&FuncList);
-		if (rv != CKR_OK) throw rv;
-		return rv;
-	}
-	catch (int RV){
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = C_GetFuncList(&FuncList);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::Initialize() {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_Initialize pC_Initialize = FuncList->C_Initialize;
-	try {
-		CK_RV rv = pC_Initialize(NULL_PTR);
-		if (rv != CKR_OK) throw rv;
-		return rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = pC_Initialize(NULL_PTR);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::Initialize(CK_C_INITIALIZE_ARGS InitArgs) {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_Initialize pC_Initialize = FuncList->C_Initialize;
-	try {
-		CK_RV rv = pC_Initialize((CK_VOID_PTR)&InitArgs);
-		if (rv != CKR_OK) throw rv;
-		return rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = pC_Initialize((CK_VOID_PTR)&InitArgs);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::GetSlotList(CK_BBOOL token_present) {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_GetSlotList pC_GetSlotList = FuncList->C_GetSlotList;
 	CK_RV rv = pC_GetSlotList(token_present, NULL_PTR, &ListCount);
-	try {
-		if (rv != CKR_OK) throw rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
-	try {
-		if (rv != CKR_OK) throw rv;
-		SlotList = new CK_SLOT_ID;
-		rv = pC_GetSlotList(token_present, SlotList, &ListCount);
-		return rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	if (rv != CKR_OK) throw RetVal(rv);
+	SlotList = new CK_SLOT_ID;
+	rv = pC_GetSlotList(token_present, SlotList, &ListCount);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::GetSlotInfo(unsigned int slot) {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_GetSlotInfo pC_GetSlotInfo = FuncList->C_GetSlotInfo;
-	try {
-		CK_RV rv = pC_GetSlotInfo(SlotList[slot - 1], &SlotInfo);
-		if (rv != CKR_OK) throw rv;
-		return rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = pC_GetSlotInfo(SlotList[slot - 1], &SlotInfo);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::GetTokenInfo(unsigned int slot) {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_GetTokenInfo pC_GetTokenInfo = FuncList->C_GetTokenInfo;
-	try {
-		CK_RV rv = pC_GetTokenInfo(SlotList[slot - 1], &TokenInfo);
-		if (rv != CKR_OK) throw rv;
-		return rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = pC_GetTokenInfo(SlotList[slot - 1], &TokenInfo);
+	if (rv != CKR_OK)throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::Finalize() {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_Finalize pC_Finalize = FuncList->C_Finalize;
-	try {
-		CK_RV rv = pC_Finalize(NULL_PTR);
-		if (rv != CKR_OK) throw rv;
-		return rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = pC_Finalize(NULL_PTR);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::WaitForSlotEvent() {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_WaitForSlotEvent pC_WaitForSlotEvent = FuncList->C_WaitForSlotEvent;
-	try {
-		CK_RV rv = pC_WaitForSlotEvent(flags, SlotList, pReserved);
-		if (rv != CKR_OK) throw rv;
-		return rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = pC_WaitForSlotEvent(flags, SlotList, pReserved);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::GetMechanismList() {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_GetMechanismList pC_GetMechanismList = FuncList->C_GetMechanismList;
-	try {
-		CK_RV rv = pC_GetMechanismList(*SlotList, NULL_PTR, &ListCount);
-		if (rv != CKR_OK) throw rv;
-	}
-	catch (int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
-	try {
-		if (ListCount > 0) MechanismList = new CK_MECHANISM_TYPE;
-		CK_RV rv = pC_GetMechanismList(*SlotList, MechanismList, &ListCount);
-		if (rv != CKR_OK) throw rv;
-	}
-	catch(int RV) {
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = pC_GetMechanismList(*SlotList, NULL_PTR, &ListCount);
+	if (rv != CKR_OK)throw RetVal(rv);
+	if (ListCount > 0) MechanismList = new CK_MECHANISM_TYPE;
+	rv = pC_GetMechanismList(*SlotList, MechanismList, &ListCount);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 int CryptoToken::GetMechanismInfo(unsigned int slot) {
+	if (FuncList == NULL) throw FuncListErr();
 	CK_C_GetMechanismInfo pC_GetMechanismInfo = FuncList->C_GetMechanismInfo;
-	try {
-		CK_RV rv = pC_GetMechanismInfo(*SlotList, MechanismList[slot], &MechanismInfo);
-		if (rv != CKR_OK) throw rv;
-		return rv;
-	}
-	catch (int RV){
-		std::cout << "Return value is not CKR_OK (Code " << RV << " )";
-	}
+	CK_RV rv = pC_GetMechanismInfo(*SlotList, MechanismList[slot], &MechanismInfo);
+	if (rv != CKR_OK) throw RetVal(rv);
+	return rv;
 }
 
 CryptoToken::~CryptoToken() {
@@ -205,7 +145,30 @@ CryptoToken::~CryptoToken() {
 }
 
 int main() {
-	CryptoToken a(L"C:\\SoftHSM2\\lib\\softhsm2-x64.dl");
-	a.GetFunctionList();
-	if (a.Initialize() == CKR_OK) std::cout << "1";
+	
+	CryptoToken a(L"C:\\SoftHSM2\\lib\\softhsm2-x64.dll");
+	try {
+		a.GetFunctionList();
+	}
+	catch (LibLoadErr ex) {
+		std::cout << ex.what();
+		exit(1);
+	}
+	catch (FuncLoadErr ex) {
+		std::cout << ex.what();
+		exit(1);
+	}
+	catch (RetVal ex) {
+		std::cout << ex.what();
+		exit(1);
+	}
+	a.Initialize();
+	a.GetSlotList(FALSE);
+	try {
+		a.GetSlotInfo(100);
+	}
+	catch (RetVal ex) {
+		std::cout << ex.what();
+		exit(1);
+	}
 }
